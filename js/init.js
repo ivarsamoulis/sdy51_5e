bb1beda5c8fefc7a9610aea5a7757387dee84b3d
mbarmpop <mbarmpop@users.noreply.github.com>
2019-05-17 11:01:49 +0300

    new branch

---
+++ js/init.js
@@ -0,0 +1,12 @@
+ 
+    // Initialize Firebase
+    var config = {
+    apiKey: "AIzaSyAlW9YD1FZMq5Xw_E5bEHxiiU-eGhyxEkI",
+    authDomain: "sdy-51.firebaseapp.com",
+    databaseURL: "https://sdy-51.firebaseio.com",
+    projectId: "sdy-51",
+    storageBucket: "sdy-51.appspot.com",
+    messagingSenderId: "134979532230"
+    };
+    firebase.initializeApp(config);
+  
